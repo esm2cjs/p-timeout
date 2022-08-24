a36c9dde9cc790080bbe922b83a8986c2db532fb
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-08-24 13:56:16 +0200

    feat: convert to hybrid package

---
+++ .github/test-scripts/test.cjs
@@ -0,0 +1,4 @@
+const pTimeout = require("@esm2cjs/p-timeout").default;
+const assert = require("assert");
+
+assert(typeof pTimeout === "function");
---
+++ .github/test-scripts/test.mjs
@@ -0,0 +1,4 @@
+import pTimeout from "@esm2cjs/p-timeout";
+import assert from "assert";
+
+assert(typeof pTimeout === "function");
